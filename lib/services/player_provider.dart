2d7c439614f26d39daea8df0a2d2d4ebaaf05a0e
anglesen1120 <tuan.nguyenminh2507@gmail.com>
2021-03-04 22:44:07 +0700

    update provider for app

--- lib/services/player_provider.dart
+++ lib/services/player_provider.dart
@@ -1,3 +1,26 @@
 import 'package:flutter/material.dart';
+import '../model/radio.dart';
+import 'db_download_services.dart';
 
-class PlayerProviderState with ChangeNotifier {}
+enum RadioPlayerState { LOADING, STOPPED, PLAYING, PAUSED, COMPLETE }
+
+class PlayerProvider with ChangeNotifier {
+  List<RadioModel> _radiosFetcher;
+  List<RadioModel> get allRadio => _radiosFetcher;
+  int get totalRecords => _radiosFetcher != null ? _radiosFetcher.length : 0;
+  getPlayerState() => _playerState;
+
+  RadioPlayerState _playerState = RadioPlayerState.STOPPED;
+  PlayerProvider() {
+    _radiosFetcher = List<RadioModel>();
+  }
+
+  fetchAllRadios() async {
+    _radiosFetcher = await DBDownloadServices.fetchLocalDB();
+  }
+
+  void updatePlayerState(RadioPlayerState radioPlayerState) {
+    _playerState = radioPlayerState;
+    notifyListeners();
+  }
+}
